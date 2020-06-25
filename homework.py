8c15a4c667212af1d2bfa7d30b77d0f68b6a92fb
azimbek01 <azimb3k@yandex.ru>
2020-06-25 19:27:26 +0600

    Изменены homework.py, gitignore

--- homework.py
+++ homework.py
@@ -4,44 +4,90 @@ import telegram
 import time
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN") 
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN') 
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID') 
+
+bot = telegram.Bot(TELEGRAM_TOKEN) 
+
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/' 
+
+bot = telegram.Bot(TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
+<<<<<<< HEAD
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        raise RuntimeError('Имя задания неопределено - None')
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        if homework.get('status') is None:
+            raise RuntimeError(
+                f'Неопределенный статус у задания {homework_name}'
+            )
+        else:
+            if homework['status'] == 'rejected':
+                verdict = 'К сожалению в работе нашлись ошибки.'
+            elif homework['status'] == 'approved':
+                verdict = 'Ревьюеру всё понравилось, можно ' \
+                           'приступать к следующему уроку.'
+            else:
+                raise RuntimeError(
+                    f'Неизвестный статус у задания {homework_name}'
+                )
+
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    headers = {
+        'Authorization': f'OAuth {PRACTICUM_TOKEN}'
+    }
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+=======
+    return 'Test'
+
+
+def get_homework_statuses(current_timestamp):
+    URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {
+        'Authorization': f'OAuth {PRACTICUM_TOKEN}'
+    }
+>>>>>>> de1ec20b34c3fad2dccb53ffa0f5c4ca3d06a555
+    data = {
+        'from_date': current_timestamp
+    }
+
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=data)
+        return homework_statuses.json()
+    except Exception as e:
+        print(f'Ошибка подключения к сайту: {e}')
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    return bot.send_message(
+        chat_id=CHAT_ID,
+        text=message
+    )
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0])
+                    )
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(900)
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
